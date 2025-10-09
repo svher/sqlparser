39779a16ab98cb9275cce632a64151054892ac4c
Zhu <13400521+svher@users.noreply.github.com>
2025-10-09 16:59:28 +0800

    Simplify rewrite tests (#3)

--- rewrite_test.go
+++ rewrite_test.go
@@ -7,7 +7,8 @@ import (
 	"testing"
 )
 
-const authorShopOriginalSQL = `SELECT  DISTINCT point1_id,
+func TestRewriteShopAuthorEdgeQueries(t *testing.T) {
+	rewritten := rewriteSqls(t, `SELECT  DISTINCT point1_id,
         point2_id,
         point1_type,
         point2_type,
@@ -27,9 +28,12 @@ FROM    (
         FROM    dm_temai.shop_gandalf_v1_3_graph_structure_di
         WHERE   date = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di')
         and edge_type = 'shop_sell_author_1d'
-        ) a`
+        ) a`)
+	t.Log(rewritten)
+}
 
-const multiOriginalSQL = `SELECT  DISTINCT point1_id,
+func TestRewritePointEdgeMultiStatement(t *testing.T) {
+	rewritten := rewriteSqls(t, `SELECT  DISTINCT point1_id,
         point2_id,
         point1_type,
         point2_type,
@@ -62,50 +66,6 @@ SELECT  DISTINCT point1_id,
 FROM    (
         SELECT  src AS point2_id,
                 tgt AS point1_id,
-                'shop' AS point2_type,
-                'sim' AS point1_type,
-                '' AS value,
-                (UNIX_TIMESTAMP() * 1000000) AS ts_us,
-                'sim_shop' AS edge_type,
-                ratio_src AS order_rate_weight
-        FROM    dm_temai.shop_gandalf_v1_3_graph_structure_di
-        WHERE   date = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di')
-        AND     edge_type = 'shop_sell_sim_1d'
-        ) a;
-
-SELECT  DISTINCT point1_id,
-        point2_id,
-        point1_type,
-        point2_type,
-        value,
-        ts_us,
-        edge_type,
-        cast(order_rate_weight as float)
-FROM    (
-        SELECT  src AS point1_id,
-                tgt AS point2_id,
-                'author' AS point1_type,
-                'sim' AS point2_type,
-                '' AS value,
-                (UNIX_TIMESTAMP() * 1000000) AS ts_us,
-                'author_sim' AS edge_type,
-                ratio_src AS order_rate_weight
-        FROM    dm_temai.shop_gandalf_v1_3_graph_structure_di
-        WHERE   date = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di')
-        AND     edge_type = 'author_sell_sim_1d'
-        ) a;
-
-SELECT  DISTINCT point1_id,
-        point2_id,
-        point1_type,
-        point2_type,
-        value,
-        ts_us,
-        edge_type,
-        cast(order_rate_weight as float)
-FROM    (
-        SELECT  src AS point2_id,
-                tgt AS point1_id,
                 'author' AS point2_type,
                 'sim' AS point1_type,
                 '' AS value,
@@ -115,11 +75,12 @@ FROM    (
         FROM    dm_temai.shop_gandalf_v1_3_graph_structure_di
         WHERE   date = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di')
         AND     edge_type = 'author_sell_sim_1d'
-        ) a;`
+        ) a;`)
+	t.Log(rewritten)
+}
 
-const multiRewrittenSQL = "select distinct named_struct('id', cast(point1_id as string)) as outv_pk_prop, cast(point2_id as string) as bg__id, point1_type as outv_label, point2_type as bg__bg__label, edge_type as label, cast(order_rate_weight as float) as order_rate_weight from (select src as point1_id, tgt as point2_id, 'shop' as point1_type, 'sim' as point2_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'shop_sim' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'shop_sell_sim_1d') as a;\nselect distinct named_struct('id', cast(point1_id as string)) as outv_pk_prop, cast(point2_id as string) as bg__id, point1_type as outv_label, point2_type as bg__bg__label, edge_type as label, cast(order_rate_weight as float) as order_rate_weight from (select src as point2_id, tgt as point1_id, 'shop' as point2_type, 'sim' as point1_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'sim_shop' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'shop_sell_sim_1d') as a;\nselect distinct named_struct('id', cast(point1_id as string)) as outv_pk_prop, cast(point2_id as string) as bg__id, point1_type as outv_label, point2_type as bg__bg__label, edge_type as label, cast(order_rate_weight as float) as order_rate_weight from (select src as point1_id, tgt as point2_id, 'author' as point1_type, 'sim' as point2_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'author_sim' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'author_sell_sim_1d') as a;\nselect distinct named_struct('id', cast(point1_id as string)) as outv_pk_prop, cast(point2_id as string) as bg__id, point1_type as outv_label, point2_type as bg__bg__label, edge_type as label, cast(order_rate_weight as float) as order_rate_weight from (select src as point2_id, tgt as point1_id, 'author' as point2_type, 'sim' as point1_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'sim_author' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'author_sell_sim_1d') as a"
-
-const pointOriginalSQL = `SELECT
+func TestRewritePointStatementHelper(t *testing.T) {
+	rewritten := rewriteSqls(t, `SELECT
   sim_id as point_id,
   'sim' as point_type,
   sim_id as point_value,
@@ -209,112 +170,11 @@ const pointOriginalSQL = `SELECT
 FROM
   dm_temai.sim_product_feature_aggregation_new_df
 WHERE
-  date = max_pt('dm_temai.sim_product_feature_aggregation_new_df')`
-
-const pointRewrittenSQL = "select 'sim' as label, cast(sim_id as string) as id, prod_cnt, second_cid_new, shop_cnt, create_prod_cnt_90d, update_prod_cnt_90d, dx_prod_cnt_his, banned_product_cnt_90d, banned_product_cnt_td, delete_product_cnt_90d, delete_product_cnt_td, banned_product_ratio_td, banned_product_ratio_90d, delete_product_ratio_td, delete_product_ratio_90d, prod_bad_eval_ratio_td, prod_bad_eval_ratio_30d, prod_good_eval_ratio_td, prod_good_eval_ratio_30d, product_quality_refund_order_ratio_td, product_quality_refund_order_ratio_30d, complain_compl_ratio_200d, live_click_show_raito_30d, live_d_o_ratio_30d, explain_ucnt_30d, live_ucnt_30d, ecqc_refuse_ratio, cast(ecqc_refuse_task_cnt as double), tcs_refuse_ratio, machine_audit_fail_ratio, machine_audit_fail_cnt, audit_fail_ratio, audit_fail_cnt, second_cid_conflict_cnt, second_cid_conflict_ratio, first_cid_conflict_cnt, first_cid_conflict_ratio, order_cnt, ccr_order_cnt, quality_ccr_order_cnt, content_ccr_order_cnt, service_ccr_order_cnt, p0_ccr_order_cnt, bad_comment_order_cnt, good_comment_order_cnt, quality_return_order_cnt, complaint_order_cnt, author_cnt, banned_product_cnt, delete_product_cnt, clear_shop_cnt, low_level_shop_cnt, low_score_shop_cnt, low_score_author_cnt, no_auth_author_cnt, low_level_author_cnt, comment_order_cnt, ccr_ratio, quality_ccr_ratio, content_ccr_ratio, service_ccr_ratio, p0_ccr_ratio, bad_comment_ratio, good_comment_ratio, quality_return_order_ratio, complaint_order_ratio, banned_product_ratio, delete_product_ratio, clear_shop_ratio, low_level_shop_ratio, low_score_shop_ratio, no_auth_author_ratio, low_score_author_ratio, low_level_author_ratio, ccr_ratio_pct_second, quality_ccr_ratio_pct_second, content_ccr_ratio_pct_second, service_ccr_ratio_pct_second, p0_ccr_ratio_pct_second, bad_comment_ratio_pct_second, good_comment_ratio_pct_second, quality_return_ratio_pct_second, complaint_ratio_pct_second, prod_set from dm_temai.sim_product_feature_aggregation_new_df where `date` = max_pt('dm_temai.sim_product_feature_aggregation_new_df')"
-
-func TestRewriteShopAuthorEdgeQueries(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name           string
-		input          string
-		wantNormalized string
-		wantRewritten  string
-	}{{
-		name:           "author_to_shop",
-		input:          authorShopOriginalSQL,
-		wantNormalized: "select distinct point1_id, point2_id, point1_type, point2_type, value, ts_us, edge_type, cast(order_rate_weight as float) from (select src as point2_id, tgt as point1_id, 'shop' as point2_type, 'author' as point1_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'author_shop' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'shop_sell_author_1d') as a",
-		wantRewritten:  "select distinct named_struct('id', cast(point1_id as string)) as outv_pk_prop, cast(point2_id as string) as bg__id, point1_type as outv_label, point2_type as bg__bg__label, edge_type as label, cast(order_rate_weight as float) as order_rate_weight from (select src as point2_id, tgt as point1_id, 'shop' as point2_type, 'author' as point1_type, '' as value, (UNIX_TIMESTAMP() * 1000000) as ts_us, 'author_shop' as edge_type, ratio_src as order_rate_weight from dm_temai.shop_gandalf_v1_3_graph_structure_di where `date` = max_pt('dm_temai.shop_gandalf_v1_3_graph_structure_di') and edge_type = 'shop_sell_author_1d') as a",
-	}}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			stmt, err := Parse(tt.input)
-			if err != nil {
-				t.Fatalf("Parse(%s) error: %v", tt.name, err)
-			}
-
-			if got := String(stmt); got != tt.wantNormalized {
-				t.Fatalf("normalized SQL mismatch\nwant: %s\n got: %s", tt.wantNormalized, got)
-			}
-
-			sel, ok := stmt.(*Select)
-			if !ok {
-				t.Fatalf("expected *Select, got %T", stmt)
-			}
-
-			rewritePointEdgeSelect(t, sel)
-
-			if got := String(sel); got != tt.wantRewritten {
-				t.Fatalf("rewritten SQL mismatch\nwant: %s\n got: %s", tt.wantRewritten, got)
-			}
-		})
-	}
+  date = max_pt('dm_temai.sim_product_feature_aggregation_new_df')`)
+	t.Log(rewritten)
 }
 
-func TestRewritePointEdgeMultiStatement(t *testing.T) {
-	t.Parallel()
-
-	got := rewritePointEdgeStatements(t, multiOriginalSQL)
-	if got != multiRewrittenSQL {
-		t.Fatalf("multi-statement rewrite mismatch\nwant: %s\n got: %s", multiRewrittenSQL, got)
-	}
-}
-
-func TestRewritePointStatementHelper(t *testing.T) {
-	t.Parallel()
-
-	if got := rewritePointEdgeStatements(t, pointOriginalSQL); got != pointRewrittenSQL {
-		t.Fatalf("point rewrite mismatch\nwant: %s\n got: %s", pointRewrittenSQL, got)
-	}
-}
-
-func TestRewritePointQueries(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name           string
-		input          string
-		wantNormalized string
-		wantRewritten  string
-	}{{
-		name:           "sim_point",
-		input:          pointOriginalSQL,
-		wantNormalized: "select sim_id as point_id, 'sim' as point_type, sim_id as point_value, prod_cnt, second_cid_new, shop_cnt, create_prod_cnt_90d, update_prod_cnt_90d, dx_prod_cnt_his, banned_product_cnt_90d, banned_product_cnt_td, delete_product_cnt_90d, delete_product_cnt_td, banned_product_ratio_td, banned_product_ratio_90d, delete_product_ratio_td, delete_product_ratio_90d, prod_bad_eval_ratio_td, prod_bad_eval_ratio_30d, prod_good_eval_ratio_td, prod_good_eval_ratio_30d, product_quality_refund_order_ratio_td, product_quality_refund_order_ratio_30d, complain_compl_ratio_200d, live_click_show_raito_30d, live_d_o_ratio_30d, explain_ucnt_30d, live_ucnt_30d, ecqc_refuse_ratio, cast(ecqc_refuse_task_cnt as double), tcs_refuse_ratio, machine_audit_fail_ratio, machine_audit_fail_cnt, audit_fail_ratio, audit_fail_cnt, second_cid_conflict_cnt, second_cid_conflict_ratio, first_cid_conflict_cnt, first_cid_conflict_ratio, order_cnt, ccr_order_cnt, quality_ccr_order_cnt, content_ccr_order_cnt, service_ccr_order_cnt, p0_ccr_order_cnt, bad_comment_order_cnt, good_comment_order_cnt, quality_return_order_cnt, complaint_order_cnt, author_cnt, banned_product_cnt, delete_product_cnt, clear_shop_cnt, low_level_shop_cnt, low_score_shop_cnt, low_score_author_cnt, no_auth_author_cnt, low_level_author_cnt, comment_order_cnt, ccr_ratio, quality_ccr_ratio, content_ccr_ratio, service_ccr_ratio, p0_ccr_ratio, bad_comment_ratio, good_comment_ratio, quality_return_order_ratio, complaint_order_ratio, banned_product_ratio, delete_product_ratio, clear_shop_ratio, low_level_shop_ratio, low_score_shop_ratio, no_auth_author_ratio, low_score_author_ratio, low_level_author_ratio, ccr_ratio_pct_second, quality_ccr_ratio_pct_second, content_ccr_ratio_pct_second, service_ccr_ratio_pct_second, p0_ccr_ratio_pct_second, bad_comment_ratio_pct_second, good_comment_ratio_pct_second, quality_return_ratio_pct_second, complaint_ratio_pct_second, prod_set from dm_temai.sim_product_feature_aggregation_new_df where `date` = max_pt('dm_temai.sim_product_feature_aggregation_new_df')",
-		wantRewritten:  pointRewrittenSQL,
-	}}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			stmt, err := Parse(tt.input)
-			if err != nil {
-				t.Fatalf("Parse(%s) error: %v", tt.name, err)
-			}
-
-			if got := String(stmt); got != tt.wantNormalized {
-				t.Fatalf("normalized SQL mismatch\nwant: %s\n got: %s", tt.wantNormalized, got)
-			}
-
-			sel, ok := stmt.(*Select)
-			if !ok {
-				t.Fatalf("expected *Select, got %T", stmt)
-			}
-
-			rewritePointEdgeSelect(t, sel)
-
-			if got := String(sel); got != tt.wantRewritten {
-				t.Fatalf("rewritten SQL mismatch\nwant: %s\n got: %s", tt.wantRewritten, got)
-			}
-		})
-	}
-}
-
-func rewritePointEdgeStatements(t testing.TB, sql string) string {
-	t.Helper()
-
+func rewriteSqls(t testing.TB, sql string) string {
 	tokenizer := NewStringTokenizer(sql)
 	var rewritten []string
 	for {
@@ -332,28 +192,25 @@ func rewritePointEdgeStatements(t testing.TB, sql string) string {
 		if !ok {
 			t.Fatalf("unexpected statement type %T", stmt)
 		}
-		rewritePointEdgeSelect(t, selectStmt)
+		rewriteSql(t, selectStmt)
 		rewritten = append(rewritten, String(selectStmt))
 	}
 
 	return strings.Join(rewritten, ";\n")
 }
-func rewritePointEdgeSelect(t testing.TB, sel *Select) {
-	t.Helper()
 
-	if rewriteEdgeSelect(t, sel) {
+func rewriteSql(t testing.TB, sel *Select) {
+	if rewriteEdgeSql(t, sel) {
 		return
 	}
-	if rewritePointSelect(t, sel) {
+	if rewritePointSql(t, sel) {
 		return
 	}
 
 	t.Fatalf("select does not contain recognizable point or edge columns")
 }
 
-func rewriteEdgeSelect(t testing.TB, sel *Select) bool {
-	t.Helper()
-
+func rewriteEdgeSql(t testing.TB, sel *Select) bool {
 	var (
 		point1ID   *AliasedExpr
 		point2ID   *AliasedExpr
@@ -432,9 +289,7 @@ func rewriteEdgeSelect(t testing.TB, sel *Select) bool {
 	return true
 }
 
-func rewritePointSelect(t testing.TB, sel *Select) bool {
-	t.Helper()
-
+func rewritePointSql(t testing.TB, sel *Select) bool {
 	var (
 		pointID   *AliasedExpr
 		pointType *AliasedExpr
@@ -495,7 +350,6 @@ func aliasOrColumnName(ae *AliasedExpr) string {
 }
 
 func newAliasedExprFromString(t testing.TB, expr, alias string) *AliasedExpr {
-	t.Helper()
 	parsedExpr := mustParseExpr(t, expr)
 	return &AliasedExpr{
 		Expr: parsedExpr,
@@ -518,7 +372,6 @@ func deriveAliasFromExpr(expr Expr) string {
 }
 
 func mustParseExpr(t testing.TB, expr string) Expr {
-	t.Helper()
 	stmt, err := Parse("select " + expr)
 	if err != nil {
 		t.Fatalf("Parse expression %q: %v", expr, err)
